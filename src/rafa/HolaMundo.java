9251d7ed8ed4342e71ad2069384e6652c6394fd0
Jose Luis <jarias7997@ceuandalucia.es>
2022-11-30 20:08:14 +0100

    Jose Luis

--- src/rafa/HolaMundo.java
+++ src/rafa/HolaMundo.java
@@ -2,6 +2,6 @@ package rafa;
 
 public class HolaMundo {
 public static void main(String[] args) {
-	System.out.println("Hola mundo soy Jose Luis");
+	System.out.println("Hola mundo soy dJose Luis");
 }
 }
